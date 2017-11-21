452593add15230093bb5ac371afbdd3e32463408
Danil Razumkov <razumkov@e1.ru>
2017-11-21 20:29:13 +0500

    123

--- server.js
+++ server.js
@@ -19,5 +19,8 @@ sok.on('connection', function (socket) {
     socket.send('Добро пожаловать!');
         socket.on('message', function(data){
         socket.broadcast.send(data);
+        socket.broadcast.send(data);
+        socket.broadcast.send(data);
+        socket.broadcast.send(data);
     });
 });
