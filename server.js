40b20d28ad2191cbb2cf7eaa1513fc45dd3cca4e
Danil Razumkov <razumkov@e1.ru>
2017-11-22 16:17:13 +0500

    123

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var body = require('body-parser');
 var sok = require('socket.io')(3001);
 const mysql = require('mysql');
 var server = express();
-
+//Подключение к бд
 var connection = mysql.createConnection({
   host     : '5.63.158.31',
   user     : 'gus',
@@ -11,8 +11,10 @@ var connection = mysql.createConnection({
   database : 'my_base'
 });
 
+//установление соединения
 connection.connect();
 console.log('Ок');
+
 
 server.listen('3000', function () {
     console.log('ПУСК!');
@@ -20,6 +22,18 @@ server.listen('3000', function () {
 sok.on('connection', function (socket) {
     socket.send('Все знают что Андрей не купил Данилу мясо!');
         socket.on('message', function(data){
-        socket.broadcast.send(data);
+          var countbd = connection.query("SELECT COUNT(1) FROM `user`", function(error, result, f){
+            if(!(error)){
+              var count = result[0]['COUNT(1)'];
+              var user = connection.query("SELECT * FROM `user`", function(error, result, f){
+                if(!(error)){
+                  for (var i = 0; i < count; i++) {
+                    socket.broadcast.send(result[i]['login']+data);
+                    console.log();
+                  }
+                }
+              });
+            }
+          });
     });
 });
