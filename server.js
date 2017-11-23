4718e18a2cfa84d8f7ba63c6f00620eba98458b9
Danil Razumkov <razumkov@e1.ru>
2017-11-23 17:52:01 +0500

    123

--- server.js
+++ server.js
@@ -23,11 +23,12 @@ server.listen('3000', function () {
 
 sok.on('connection', function (socket) {
     socket.send('Все знают что Андрей не купил Данилу мясо!');
-});
 
-sok.on('message', function(data){
-  var pred_msg = JSON.parse(data);
-  var msg = result[i]['login']+": "+pred_msg;
-  socket.broadcast.send(msg);
-  console.log(msg);
+    socket.on('message', function(data){
+      var pred_msg = JSON.parse(data);
+      var msg = result[i]['login']+": "+pred_msg;
+      socket.broadcast.send(msg);
+      console.log(msg);
+
+    });
 });
