ccfc6527cfa6dd0c5256f1824165be8d73c017ec
Danil Razumkov <razumkov@e1.ru>
2017-12-01 16:34:16 +0500

    123

--- server.js
+++ server.js
@@ -20,9 +20,11 @@ server.listen('3000', function () {
     console.log('ПУСК!');
 });
 
+var click = 0;
 
 sok.on('connection', function (socket) {
-    socket.send('{"type" : "chat_message", "message" : "Добро подажаловать! Андрей купи мясо..."}');
+  var local_click = 0;
+    socket.send('{"type" : "chat_message", "message" : "Добро подажаловать! Андрей купи мясо..., уже не надо!"}');
 
     socket.on('message', function(data){
       var pred_msg = JSON.parse(data);
@@ -32,6 +34,14 @@ sok.on('connection', function (socket) {
       }
       else if(pred_msg.type == "online_message"){
         var send = '{"type" : "online_message", "message" : "'+pred_msg.message+'"}';
+        socket.broadcast.send(send);
+      }
+      else if(pred_msg.type == "click_message"){
+        local_click++;
+        click++;
+        var send_l = '{"type" : "click_message_local", "message" : "'+local_click+'"}';
+        var send = '{"type" : "click_message", "message" : "'+click+'"}';
+        socket.broadcast.send(send_l);
         socket.broadcast.send(send);
       }
 
